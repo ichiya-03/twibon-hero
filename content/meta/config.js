01c54c78b56f8982cb6a13592fe7ec10cea1d381
Arifumi Matsumoto <arifumi@ariair3.local>
2018-12-05 23:46:27 +0900

    initial commit

---
+++ content/meta/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: "ツイ盆展サイト", // <title>
+  shortSiteTitle: "ツイ盆展 - Twitter Bonsai Exhibition", // <title> ending for posts and pages
+  siteDescription: "『ツイ盆展』はTwitter上で開催するオンライン盆栽展です。 初心者さんからベテランさんまで間口は広くゆるく！",
+  siteUrl: "https://twibon.cf",
+  // pathPrefix: "",
+  siteImage: "twibon-banner.png",
+  siteLanguage: "jp",
+
+  /* author */
+  authorName: "ツイ盆展",
+  authorTwitterAccount: "twibonten",
+
+  /* info */
+  headerTitle: "ツイ盆展",
+  headerSubTitle: "ツイッター盆栽展",
+
+  /* manifest.json */
+  manifestName: "ツイ盆展 - Twitter Bonsai Exhibition",
+  manifestShortName: "twibonten", // max 12 characters
+  manifestStartUrl: "/index.html",
+  manifestBackgroundColor: "white",
+  manifestThemeColor: "#666",
+  manifestDisplay: "standalone",
+
+  // gravatar
+  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
+  // Replace your email adress with md5-code.
+  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
+  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
+  gravatarImgMd5: "",
+
+  // social
+  authorSocialLinks: [
+    { name: "twitter", url: "https://twitter.com/twibonten" },
+  ]
+};
---
+++ content/parts/footnote.md
@@ -0,0 +1,6 @@
+---
+title: footnote
+---
+
+* delivered by [Netlify](https://www.netlify.com/)
+* graphic by [hyakumanga.com](http://hyakumanga.com)
